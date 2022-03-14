5e4883bb27c51fda840c0f69e66830c47f60e00c
jaco <valentinjacofskyort@gmail.com>
2022-03-13 22:16:24 -0300

    Google SingIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken")
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generarJWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -63,11 +64,62 @@ const login = async(req = request, res = response) => {
         })
     }
 
+}
+
+const googleSingIn = async(req = request, res = response) => {
+    
+    const {id_token} = req.body
+
+    try {
+
+        const {email, nombre, img} = await googleVerify(id_token)
+        
+        let usuario = await Usuario.findOne({email})
+        
+        // Usuario no existe
+        if (!usuario) {
+            const data = {
+                nombre,
+                email,
+                password: ':P',
+                rol: 'USER_ROLE',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario(data)
+            await usuario.save()
+        }
+        
+        // Si el usuario en DB gogle false
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+        
+        console.log(usuario)
+        // Generar JWT
+        const token = await generarJWT(usuario.id)
+
+        res.status(200).json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
 
     
+
 }
 
 
 module.exports = {
     login,
+    googleSingIn
 }
